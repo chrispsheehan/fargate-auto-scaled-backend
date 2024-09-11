1fcd30ec2c379172756442964451d32f31903c19
chrispsheehan <chrispsheehan@gmail.com>
2024-09-11 19:16:20 +0100

    do not hang on ecs error

--- tf/main.tf
+++ tf/main.tf
@@ -105,7 +105,7 @@ resource "aws_ecs_service" "ecs" {
   cluster               = aws_ecs_cluster.cluster.id
   task_definition       = aws_ecs_task_definition.task.arn
   desired_count         = var.initial_task_count
-  wait_for_steady_state = true
+  wait_for_steady_state = false  # Disable waiting for steady state to enable iteration  ##########
 
   deployment_circuit_breaker {
     enable   = true
