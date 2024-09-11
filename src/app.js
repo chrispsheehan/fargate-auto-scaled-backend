8853ca09e7b5970ce6da272899fb9da53998992e
chrispsheehan <chrispsheehan@gmail.com>
2024-09-11 19:34:03 +0100

    log all requests

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ const port = process.env.PORT;
 
 app.listen(port)
 
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  next();
+});
+
 app.get('/hello', (req, res) => {
   res.status(200).json({msg: "Hello, this is your API"});
 });
