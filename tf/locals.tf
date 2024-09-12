1405681cf88966e87fe26198f3b6098f2fc2f3fd
chrispsheehan <chrispsheehan@gmail.com>
2024-09-12 10:18:01 +0100

    add / to log group name

--- tf/locals.tf
+++ tf/locals.tf
@@ -1,7 +1,7 @@
 locals {
   formatted_name      = replace(var.project_name, "-", "_")
   az_count            = min(length(data.aws_availability_zones.azs.names), var.max_az)
-  cloudwatch_log_name = "ecs/${local.formatted_name}"
+  cloudwatch_log_name = "/ecs/${local.formatted_name}"
   container_definitions = templatefile("${path.module}/container_definitions.tpl", {
     container_name      = var.project_name
     image_uri           = var.image_uri
