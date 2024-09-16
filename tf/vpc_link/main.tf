3e5812c6acab2da3baeeefa1b89d8b1fc47e05fe
chrispsheehan <chrispsheehan@gmail.com>
2024-09-16 17:04:14 +0100

    vpc logs fixes

--- tf/vpc_link/main.tf
+++ tf/vpc_link/main.tf
@@ -1,8 +1,19 @@
+resource "aws_iam_role" "example" {
+  name               = "example"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy" "example" {
+  name   = "example"
+  role   = aws_iam_role.example.id
+  policy = data.aws_iam_policy_document.example.json
+}
+
 resource "aws_flow_log" "vpc_flow_log" {
-  log_destination      = aws_cloudwatch_log_group.vpc_flow_logs.arn
-  log_destination_type = "cloud-watch-logs"
-  vpc_id               = var.private_vpc_id
-  traffic_type         = "ALL"
+  iam_role_arn    = aws_iam_role.example.arn
+  log_destination = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  vpc_id          = var.private_vpc_id
+  traffic_type    = "ALL"
 }
 
 resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
@@ -11,20 +22,20 @@ resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
 }
 
 resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id            = var.private_vpc_id
-  service_name      = "com.amazonaws.${var.region}.ecr.api"
-  vpc_endpoint_type = "Interface"
-  security_group_ids = [aws_security_group.this.id]
-  subnet_ids         = var.private_subnet_ids
+  vpc_id              = var.private_vpc_id
+  service_name        = "com.amazonaws.${var.region}.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.this.id]
+  subnet_ids          = var.private_subnet_ids
   private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id            = var.private_vpc_id
-  service_name      = "com.amazonaws.${var.region}.ecr.dkr"
-  vpc_endpoint_type = "Interface"
-  security_group_ids = [aws_security_group.this.id]
-  subnet_ids         = var.private_subnet_ids
+  vpc_id              = var.private_vpc_id
+  service_name        = "com.amazonaws.${var.region}.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.this.id]
+  subnet_ids          = var.private_subnet_ids
   private_dns_enabled = true
 }
 
