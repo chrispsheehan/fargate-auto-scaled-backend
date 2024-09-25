25913985b1760b0995baf9a178f0d48964e493e5
chrispsheehan <chrispsheehan@gmail.com>
2024-09-25 15:18:38 +0100

    check job

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,3 +1,19 @@
 output "url" {
   value = "${module.vpc_link.api_invoke_url}/host"
 }
+
+output "task_definition_arn" {
+  value = module.task_definition.task_definition_arn
+}
+
+output "cluster_name" {
+  value = module.ecs.cluster_name
+}
+
+output "service_name" {
+  value = module.ecs.service_name
+}
+
+output "image_uri" {
+  value = module.task_definition.image_uri
+}
--- tf/task_definition/output.tf
+++ tf/task_definition/output.tf
@@ -1,3 +1,7 @@
 output "task_definition_arn" {
   value = aws_ecs_task_definition.task.arn
 }
+
+output "image_uri" {
+  value = local.image_uri
+}
