06f8ba2ada5e4fdc2e02dc0331b9d97cc0b24429
chrispsheehan <chrispsheehan@gmail.com>
2024-09-27 13:51:53 +0100

    data "aws_subnets"

--- tf/service/data.tf
+++ tf/service/data.tf
@@ -5,6 +5,13 @@ data "aws_vpc" "private" {
   }
 }
 
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.private.id]
+  }
+}
+
 data "aws_subnet" "subnets" {
   for_each = toset(data.aws_subnets.private.ids)
   id       = each.value
