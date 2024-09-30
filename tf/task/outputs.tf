e31e9c48e0d6875eb70d97119b4954e171a8e7e9
chrispsheehan <chrispsheehan@gmail.com>
2024-09-30 11:06:15 +0100

    output task_definition_revision

--- tf/task/outputs.tf
+++ tf/task/outputs.tf
@@ -2,6 +2,10 @@ output "task_definition_arn" {
   value = aws_ecs_task_definition.task.arn
 }
 
+output "task_definition_revision" {
+  value = aws_ecs_task_definition.task.revision
+}
+
 output "cloudwatch_log_group" {
   value = aws_cloudwatch_log_group.ecs_log_group.name
 }
