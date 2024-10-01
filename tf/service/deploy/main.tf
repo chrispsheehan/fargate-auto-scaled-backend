f045f76c744ae2da444c9aec7c634ca8f4fa5b70
chrispsheehan <chrispsheehan@gmail.com>
2024-10-01 11:05:52 +0100

    fix code deploy app reference

--- tf/service/deploy/main.tf
+++ tf/service/deploy/main.tf
@@ -14,10 +14,10 @@ resource "aws_iam_role_policy" "codedeploy_role_policy" {
 }
 
 resource "aws_codedeploy_deployment_group" "this" {
-  app_name = var.codedeploy_group_name
+  app_name = aws_codedeploy_app.ecs_app.name
   # Shifts 10% of the traffic in the first increment, then shifts the remaining 90% after 5 minutes
   deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
-  deployment_group_name  = "${var.project_name}-blue-green-group"
+  deployment_group_name  = var.codedeploy_group_name
   service_role_arn       = aws_iam_role.this.arn
 
   auto_rollback_configuration {
--- tf/variables.tfvars
+++ tf/variables.tfvars
@@ -1,6 +1,6 @@
 project_name          = "fargate-auto-scaled-backend"
 codedeploy_app_name   = "fargate-auto-scaled-backend-ecs-codedeploy"
-codedeploy_group_name = "fargate-auto-scaled-backend-ecs-deploy-group"
+codedeploy_group_name = "fargate-auto-scaled-backend-ecs-blue-green-group"
 region                = "eu-west-2"
 private_vpc_name      = "ecs-private-vpc"
 api_stage_name        = "dev"
